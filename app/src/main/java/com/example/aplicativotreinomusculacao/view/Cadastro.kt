7bba31345021cb5bd27ce5da99254bd6952f4425
Carlos Silva <caduaba@hotmail.com>
2023-12-24 02:29:38 -0300

    Commit de algumas telas iniciais

---
+++ app/src/main/java/com/example/aplicativotreinomusculacao/view/Cadastro.kt
@@ -0,0 +1,12 @@
+package com.example.aplicativotreinomusculacao.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.aplicativotreinomusculacao.R
+
+class Cadastro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro)
+    }
+}
